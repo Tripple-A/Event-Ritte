ede3009777d9302ff328e6111eaf21a3cc32b635
Biodun <biodun9@gmail.com>
2020-03-27 12:43:04 +0100

    added working routes and associated models to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    post 'events/new', to: 'events#create'
+    root 'users#index'
+    get '/signin', to: 'sessions#new'
+    post '/signin', to: 'sessions#create'
+    delete '/signout', to: 'sessions#destroy'
+    resources :users
+    resources :events
+    resources :invitations
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+  
 end
